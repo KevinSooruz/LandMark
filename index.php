aba198973e49db7f86bab836fcd14e9b1fd5f180
Kévin <k.sourrue@gmail.com>
2015-07-27 16:43:04 +0200

    Modification nom app

--- index.php
+++ index.php
@@ -6,11 +6,6 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
 <meta name="description" content="Localize">
 <meta name="keywords" content="Localize">
-<title>Spartan Training</title>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
-<meta name="description" content="Spartan Training">
-<meta name="keywords" content="Spartan Training">
 <meta name="apple-mobile-web-app-capable" content="yes" />
 <meta name="apple-touch-fullscreen" content="yes">
 <meta name="apple-mobile-web-app-status-bar-style" content="white" />
